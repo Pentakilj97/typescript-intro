2a218c10ddcec41ec36969cc2c2d10a85ca09c48
crex02 <127298119+Pentakilj97@users.noreply.github.com>
2023-05-09 10:55:14 +0200

    agg

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { ITodo } from 'src/app/model/i-todo';
 import { Todo } from 'src/app/model/todo';
+import { DataService } from 'src/app/src/data/data.service';
 import { UtilityService } from 'src/app/src/utility.service';
 
 @Component({
@@ -9,20 +11,25 @@ import { UtilityService } from 'src/app/src/utility.service';
 })
 export class TodoListComponent {
 
-  todoArray: Todo[] =[]
+  todoArray: ITodo[] =[]
 
-  constructor(private utilityService: UtilityService){
-    const todo1=new Todo('comprare il pane', false)
-    const todo2=new Todo('chiamare la nonna', false)
-    const todo3=new Todo('pagare la bolletta', false)
+  constructor(private utilityService: UtilityService, private dataS:DataService){
+    // const todo1=new Todo('comprare il pane', false)
+    // const todo2=new Todo('chiamare la nonna', false)
+    // const todo3=new Todo('pagare la bolletta', false)
 
-    this.todoArray=[todo1,todo2,todo3]
+    // this.todoArray=[todo1,todo2,todo3]
+    this.dataS.getData().then(data=>{
+      console.log(data)
+      this.todoArray=data
+      console.log(this.todoArray)
+
+    });
+
   }
 
 
-  sortTodo(){
+  sortTodo():void{
     this.todoArray=this.utilityService.sortArrayAlphabetically(this.todoArray)
   }
-
-
 }
--- src/app/src/utility.service.ts
+++ src/app/src/utility.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Todo } from '../model/todo';
+import { ITodo } from '../model/i-todo';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class UtilityService {
 
   constructor() { }
 
-  sortArrayAlphabetically(array: Todo[]): Todo[]{
+  sortArrayAlphabetically(array: ITodo[]): ITodo[]{
     const newArray=[...array]
 
     newArray.sort((a,b) => a.title.localeCompare(b.title))
